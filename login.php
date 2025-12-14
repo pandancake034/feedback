ef3658cdfc7af19f3a39edde2faf8dd862d0b0a3
pandancake034 <avinashroepa@hellofresh.nl>
2025-12-14 17:02:42 +0100

    ...

--- login.php
+++ login.php
@@ -254,7 +254,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <div style="width: 100%;">
             <div class="app-logo">
                 <span class="material-icons-outlined" style="font-size: 28px; color: var(--brand-color);">local_shipping</span>
-                LogistiekApp
+                FeedbackFlow
             </div>
             
             <h2>Welkom terug</h2>
